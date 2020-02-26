73cb2b58f4c0b802c7125e81f6efa2d37e9ce11c
Bijesh O S <bijeshos@users.noreply.github.com>
2020-02-26 10:01:03 +0530

    added init command file

---
+++ cmd/init.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+}
+
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "initialize go-cli",
+	Long:  `initialize go-cli with options`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("go-cli v0.0.1-alpha")
+
+		/*var conf string
+		conf, _ = rootCmd.Flags().GetString("config")
+		fmt.Println("config:", conf)
+		*/
+	},
+}
