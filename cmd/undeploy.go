8d76306f361e34892d603536dc67d80abc25eea5
Bijesh O S <bijeshos@users.noreply.github.com>
2020-03-07 21:39:16 +0530

    added undeploy:web command

--- cmd/undeploy.go
+++ cmd/undeploy.go
@@ -8,14 +8,26 @@ import (
 
 func init() {
 	rootCmd.AddCommand(undeployCmd)
+
+	undeployCmd.AddCommand(undeployWebCmd)
+
 }
 
 var undeployCmd = &cobra.Command{
 	Use:     "undeploy",
 	Aliases: []string{"undep", "undepl"},
 	Short:   "undeploy apps",
-	Long:    `undeploy apps using the CLI`,
+	Long:    `undeploy apps`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("executing undeploy")
+	},
+}
+
+var undeployWebCmd = &cobra.Command{
+	Use:   "web",
+	Short: "undeploy web",
+	Long:  `undeploy web`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("executing undeploy:web")
 	},
 }
