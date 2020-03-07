ce5cb72b7b51d3731e315b1258df4a9f91208177
Bijesh O S <bijeshos@users.noreply.github.com>
2020-03-07 21:39:03 +0530

    added deploy:web command

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -8,14 +8,26 @@ import (
 
 func init() {
 	rootCmd.AddCommand(deployCmd)
+
+	deployCmd.AddCommand(deployWebCmd)
+
 }
 
 var deployCmd = &cobra.Command{
 	Use:     "deploy",
 	Aliases: []string{"dep", "depl"},
 	Short:   "deploy apps",
-	Long:    `deploy apps using the CLI`,
+	Long:    `deploy apps`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("executing deploy")
+	},
+}
+
+var deployWebCmd = &cobra.Command{
+	Use:   "web",
+	Short: "deploy web",
+	Long:  `deploy web`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("executing deploy:web")
 	},
 }
