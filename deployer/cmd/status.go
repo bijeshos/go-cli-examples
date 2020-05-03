3b7a67d58046318837da6260cd1102f72d4293f7
Bijesh O S <bijeshos@users.noreply.github.com>
2020-05-03 16:08:31 +0530

    updated sub command run statements

--- deployer/cmd/status.go
+++ deployer/cmd/status.go
@@ -10,6 +10,7 @@ func init() {
 	rootCmd.AddCommand(statusCmd)
 
 	statusCmd.AddCommand(statusWebCmd)
+	statusCmd.AddCommand(statusAPICmd)
 	statusCmd.AddCommand(statusDatabaseCmd)
 }
 
@@ -17,9 +18,6 @@ var statusCmd = &cobra.Command{
 	Use:   "status",
 	Short: "status of deployed artifacts",
 	Long:  `status of deployed artifacts`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("executing status command")
-	},
 }
 
 var statusWebCmd = &cobra.Command{
@@ -27,7 +25,18 @@ var statusWebCmd = &cobra.Command{
 	Short: "status of web deployment",
 	Long:  `status of web deployment`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("executing status:web")
+		// *** add code to invoke automation end points below ***
+		fmt.Println("Executing 'deployer status web' placeholder command")
+	},
+}
+
+var statusAPICmd = &cobra.Command{
+	Use:   "api",
+	Short: "status api",
+	Long:  `status api`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// *** add code to invoke automation end points below ***
+		fmt.Println("Executing 'deployer status api' placeholder command")
 	},
 }
 
@@ -36,6 +45,7 @@ var statusDatabaseCmd = &cobra.Command{
 	Short: "status of database deployment",
 	Long:  `status of database deployment`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("executing status:database")
+		// *** add code to invoke automation end points below ***
+		fmt.Println("Executing 'deployer status database' placeholder command")
 	},
 }
