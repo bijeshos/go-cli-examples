fa4c83bdda88346651e2564a80fecfbbd431c019
Bijesh O S <bijeshos@users.noreply.github.com>
2020-05-05 12:41:22 +0530

    updated command descriptions

--- deployer/cmd/version.go
+++ deployer/cmd/version.go
@@ -12,8 +12,8 @@ func init() {
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Print the version number of deployer",
-	Long:  `Print the version number of deployer sample application`,
+	Short: "Print version number of deployer tool",
+	Long:  `This command can be used get the version number of deployer tool`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("deployer v0.0.1-alpha")
 	},
